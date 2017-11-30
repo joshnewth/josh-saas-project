f104ee2557a75b8aa926e2b0bae5e0023e1e862b
Josh Newth <joshnewth@yahoo.com>
2017-11-30 20:54:13 +0000

    Create project user association

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,9 @@ class Project < ActiveRecord::Base
   belongs_to :tenant
   validates_uniqueness_of :title
   has_many :artifacts, dependent: :destroy
-  def self.by_plan_and_tenant(tenant_id)
+  has_many :user_projects
+  has_many :users, through: :user_projects
+  def self.by_user_plan_and_tenant(tenant_id, user)
     tenant = Tenant.find(tenant_id)
     tenant.projects
   end
